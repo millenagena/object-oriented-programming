a752c018800a81227b1106e8b487fa8928c633ae
lucasmv2205 <lmartins2205@gmail.com>
2022-11-05 21:46:39 -0300

    poo2 project initial

--- TrabalhoPoo2/src/trabalhopoo2/TrabalhoPoo2.java
+++ TrabalhoPoo2/src/trabalhopoo2/TrabalhoPoo2.java
@@ -2,12 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+package trabalhopoo2;
 
 /**
  *
  * @author lucas
  */
-public class Trabalho_poo2 {
+public class TrabalhoPoo2 {
 
     /**
      * @param args the command line arguments
